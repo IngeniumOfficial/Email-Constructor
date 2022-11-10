e009c98c2c888a067b8ba4583c045e508b09b03d
yermie <yermolovpersonal@gmail.com>
2022-11-10 09:48:17 -0800

    More ui changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { createSignal, For, createEffect, Show } from 'solid-js';
+import { createSignal, For, createEffect, Show, Switch, Match, createMemo } from 'solid-js';
 import './App.scss';
 
 export default function App() {
@@ -6,6 +6,8 @@ export default function App() {
   const [details, setDetails] = createSignal({});
   const [submitted, setSubmitted] = createSignal([]); // Make sure to only bulk update this
   const [dataShow, setDataShow] = createSignal(true);
+
+  const [newLayout, setNewLayout] = createSignal(window.innerWidth); // 'regular', 'small', 'phone'
 
   const handleAdd = () => {
     setInputCount(prev => [...prev, '1']);
@@ -76,33 +78,82 @@ export default function App() {
 
 
   return (
-    <div id='app'>
-      <div id='header'>
-        <h2>Email Constructor</h2>
-      </div>
-      <div id='detailsInput'>
-        <For each={inputCount()}>{(input, index) =>
-          <InputDetails idd={index()+1} setInputCount={setInputCount()} setDetails={setDetails} details={details()} />
-        }</For>
-        <button onClick={handleAdd}>Add An Input</button>
-        <button onClick={handleRemove}>Remove An Input</button>
-        <br></br>
-        <button id='submit' onClick={handleSubmit}>Submit</button>
-        <Show when={dataShow()} fallback={<button onClick={() => setDataShow(prev => !prev)}>Show</button>}>
-          <p id='data'>
-            {JSON.stringify(details(), null, '\t')}
-          </p>
-          <button onClick={() => setDataShow(prev => !prev)}>Hide</button>
-        </Show>
-      </div>
-      <div id='generatedListOutput'>
-        <textarea>
-          {submitted().map((el) => {
-            return el+'\n';
-          })}
-        </textarea>
-      </div>
-    </div>
+    <Switch>
+      <Match when={newLayout() >= 901}>
+        <div id='app' >
+          <div id='header'>
+            <h2>Email Constructor</h2>
+          </div>
+          <div id='detailsInput'>
+            <For each={inputCount()}>{(input, index) =>
+              <InputDetails idd={index()+1} setInputCount={setInputCount()} setDetails={setDetails} details={details()} />
+            }</For>
+            <button onClick={handleAdd}>Add An Input</button>
+            <button onClick={handleRemove}>Remove An Input</button>
+            <br></br>
+            <button id='submit' onClick={handleSubmit}>Submit</button>
+            <Show when={dataShow()} fallback={
+              <>
+                <br></br>
+                <button onClick={() => setDataShow(prev => !prev)}>Show</button>
+              </>
+            }>
+              <br></br>
+              <button onClick={() => setDataShow(prev => !prev)}>Hide</button>
+              <br></br>
+              <textarea readonly id='data'>
+                {JSON.stringify(details(), null, 2)}
+              </textarea>
+            </Show>
+          </div>
+          <div id='generatedListOutput'>
+            <textarea id='output'>
+              {submitted().map((el) => {
+                return el+'\n';
+              })}
+            </textarea>
+          </div>
+        </div>
+      </Match>
+      <Match when={newLayout() <= 900}>
+      <div id='app' >
+          <div id='header'>
+            <h2>Email Constructor</h2>
+          </div>
+          <div id='lowerCollective'>
+            <div id='detailsInput'>
+              <For each={inputCount()}>{(input, index) =>
+                <InputDetails idd={index()+1} setInputCount={setInputCount()} setDetails={setDetails} details={details()} />
+              }</For>
+              <button onClick={handleAdd}>Add An Input</button>
+              <button onClick={handleRemove}>Remove An Input</button>
+              <br></br>
+              <button id='submit' onClick={handleSubmit}>Submit</button>
+              <Show when={dataShow()} fallback={
+                <>
+                  <br></br>
+                  <button onClick={() => setDataShow(prev => !prev)}>Show</button>
+                </>
+              }>
+                <br></br>
+                <button onClick={() => setDataShow(prev => !prev)}>Hide</button>
+                <br></br>
+                <textarea readonly id='data'>
+                  {JSON.stringify(details(), null, 2)}
+                </textarea>
+              </Show>
+            </div>
+            <div id='generatedListOutput'>
+              <textarea id='output'>
+                {submitted().map((el) => {
+                  return el+'\n';
+                })}
+              </textarea>
+            </div>
+          </div>
+        </div>
+      </Match>
+    </Switch>
   );
 }
 
